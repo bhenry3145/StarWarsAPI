dad35b04e39174c17f365f487d4b4ca8417dffc5
bhenry3145 <bhenry3145@gmail.com>
2025-02-01 15:22:42 -0800

    added buttons and functions

--- scripts/app.js
+++ scripts/app.js
@@ -1,1 +1,29 @@
 import { saveToLocalStorage, getFromLocalStorage, removeFromLocalStorage } from "./localStorage.js";
+
+const enterBtn = document.getElementById('enterBtn');
+const randomBtn = document.getElementById('randomBtn');
+
+const FetchData = async () => {
+    const starWarsID = document.getElementById('starWarsID').value;
+    const newID = parseInt(starWarsID);
+    const promise = await fetch(`https://www.swapi.tech/api/people/${newID}`); // max is 83
+    const data = await promise.json();
+    console.log(data);
+    return data;
+}
+
+const GenerateRandom = async () => {
+    const randomCharacter = Math.floor(Math.random() * 83) + 1;
+    const promise = await fetch(`https://www.swapi.tech/api/people/${randomCharacter}`); 
+    const data = await promise.json();
+    console.log(data);
+    return data;
+}
+
+enterBtn.addEventListener('click', () => {
+    FetchData();
+})
+
+randomBtn.addEventListener('click', () => {
+    GenerateRandom();
+})
