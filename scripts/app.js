a9144ae5c24284a9d950c263285f69ce0e933564
bhenry3145 <bhenry3145@gmail.com>
2025-02-01 17:15:36 -0800

    added more characters

--- scripts/app.js
+++ scripts/app.js
@@ -2,6 +2,17 @@ import { saveToLocalStorage, getFromLocalStorage, removeFromLocalStorage } from 
 
 const enterBtn = document.getElementById('enterBtn');
 const randomBtn = document.getElementById('randomBtn');
+const lukeBtn = document.getElementById('lukeBtn');
+const c3poBtn = document.getElementById('c3poBtn');
+const lobotBtn = document.getElementById('lobotBtn');
+const namesList = document.getElementById('namesList');
+const name = document.getElementById('name');
+const height = document.getElementById('height');
+const weight = document.getElementById('weight');
+const gender = document.getElementById('gender');
+const skinColor = document.getElementById('skinColor');
+const hairColor = document.getElementById('hairColor');
+const home = document.getElementById('home');
 
 const FetchData = async () => {
     const starWarsID = document.getElementById('starWarsID').value;
@@ -9,7 +20,37 @@ const FetchData = async () => {
     const promise = await fetch(`https://www.swapi.tech/api/people/${newID}`); // max is 83
     const data = await promise.json();
     console.log(data);
-    return data;
+    
+}
+
+const GetLuke = async () => {
+    const promise = await fetch('https://www.swapi.tech/api/people/1');
+    const data = await promise.json();
+    characterName.innerText = data.result.properties.name;
+}
+
+const GetC3PO = async () => {
+    const promise = await fetch('https://www.swapi.tech/api/people/2');
+    const data = await promise.json();
+    characterName.innerText = data.result.properties.name;
+}
+
+const GetLobot = async () => {
+    const promise = await fetch('https://www.swapi.tech/api/people/26');
+    const data = await promise.json();
+    characterName.innerText = data.result.properties.name;
+}
+
+const GetAllPeople = async () => {
+    const promise = await fetch('https://www.swapi.tech/api/people?page=1&limit=82')
+    const data = await promise.json();
+    console.log(data);
+    const peopleArray = [];
+    for (let i = 0; i < data.results.length; i++)
+    {
+        peopleArray.push(data.results[i].name);
+    }
+    
 }
 
 const GenerateRandom = async () => {
@@ -17,7 +58,7 @@ const GenerateRandom = async () => {
     const promise = await fetch(`https://www.swapi.tech/api/people/${randomCharacter}`); 
     const data = await promise.json();
     console.log(data);
-    return data;
+    
 }
 
 enterBtn.addEventListener('click', () => {
@@ -26,4 +67,16 @@ enterBtn.addEventListener('click', () => {
 
 randomBtn.addEventListener('click', () => {
     GenerateRandom();
+})
+
+lukeBtn.addEventListener('click', () => {
+    GetLuke();
+})
+
+c3poBtn.addEventListener('click', () => {
+    GetC3PO();
+})
+
+lobotBtn.addEventListener('click', () => {
+    GetLobot();
 })
